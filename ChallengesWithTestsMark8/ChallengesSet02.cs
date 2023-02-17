32cdc342c86e47260f703dd4ae21996142db14e8
Michael <keliikalani87@gmail.com>
2023-02-16 20:22:34 -0800

    week 2 challenge commit

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,137 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            foreach (char letter in lowercaseAlphabet)
+            {
+                if (letter == Char.ToLower(c))
+                {
+                    return true;
+                }
+               
+            }
+            return false;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else if (numbers.Count() == 0)
+            {
+                return 0;
+            }
+                return numbers.Min() + numbers.Max();
         }
-
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            char[] ch1 = str1.ToCharArray();
+            char[] ch2 = str2.ToCharArray();
+
+            if (ch1.Length < ch2.Length)
+            {
+                return ch1.Length;
+            }
+            else
+            {
+                return ch2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {              
+            if (numbers == null)
+            {
+                return 0;
+            }
+                return numbers.Sum();                                     
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            foreach (int i in numbers)
+            { 
+                if (i % 2 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            else if (numbers.Sum()%2 != 0)
+            {
+                return true;
+            }
+            else if (numbers.Sum() % 2 == 0)
+            {
+                return false;
+            }
+            return true;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long count = 0;
+            for (long n = 0; n < number; n++)
+            {
+                if (n % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
